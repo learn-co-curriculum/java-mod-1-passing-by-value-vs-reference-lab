10587e647a8a2634b38e19a8f9765278a6bb90ae
Katie Cleland <kcleland0818@gmail.com>
2022-08-14 17:16:29 -0600

    Re-write the lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        int firstNumber = 2;
+        int secondNumber = 5;
+
+        System.out.println("Before swapping, firstNumber = " + firstNumber + "secondNumber = " + secondNumber);
+
+        swap(firstNumber, secondNumber);
+
+        System.out.println("After swapping, firstNumber = " + firstNumber + "secondNumber = " + secondNumber);
+    }
+
+    public static void swap(int firstNumber, int secondNumber) {
+        // Write code here to swap the two values
     }
 }
